ebf6da47cab08f75ab7e6e6ed87720cddb9176d5
syednajam999 <syed.najam@vaira.app>
2021-08-07 00:21:52 +0200

    Initial Setup

---
+++ EarthQuakeLocations/EarthQuakeLocations/EarthQuakeLocationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthQuakeLocationsApp.swift
+//  EarthQuakeLocations
+//
+//  Created by Syed Najam on 07/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct EarthQuakeLocationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
